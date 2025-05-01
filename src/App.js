b2ec11b7c322268ab763f96f4be05774ca377fbf
Aneesh <aneeshg2001@gmail.com>
2025-05-01 09:12:34 +0530

    Updated App.js with counter app and animations

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+
+
 function App() {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+  const [history, setHistory] = useState([0]);
+
+  const increment = () => {
+    setCount(prev => {
+      const newCount = prev + step;
+      setHistory([...history, newCount]);
+      return newCount;
+    })
+  }
+  const decrement = () => {
+    setCount(prev => {
+      const newCount = prev - step;
+      setHistory([...history, newCount]);
+      return newCount;
+    })
+  }
+  const reset = () => {
+    setCount(0);
+    setHistory([0]);
+  }
+
+  const handleStepChange = (event) => {
+    const value = Number(event.target.value);
+    setStep(value);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> 🔢 Counter App</h1>
+      <h2>{count}</h2>
+      <div className="container">
+        <div>
+          <button onClick={increment}>➕  Increment</button>
+          <button onClick={decrement}>➖  Decrement</button>
+          <button onClick={reset}>🔄  Reset</button>
+        </div>
+        <div>
+          <label>Step: </label>
+          <input type="number" value={step} onChange={handleStepChange} min="1" />
+        </div>
+        <div>
+          <h3>History:</h3>
+          <ul>
+            {history.map((item, index) => (
+              <li className='history-item' key={index}>{item}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
